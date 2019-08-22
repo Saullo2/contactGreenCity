157d5c98bf9a7cb30af92da4cbe15f4b9fb54555
Saulo de Melo Macambira <saulomelo30@gmail.com>
2019-08-22 15:37:17 -0300

    error icons set up

--- javascript/contact.js
+++ javascript/contact.js
@@ -3,25 +3,31 @@ function errorMessages(){
     if (inputNameValidation){
 
         inputNameField = document.getElementById("nome");
+        iconNameError = document.getElementById("nome-error-icon");
         errorMessageContent = document.getElementById("nome-error-alert");
 
         pushingAsideDiv(inputNameField, errorMessageContent);
+        pushingAsideIcon(inputNameField, iconNameError);
     }
    
     if (inputEmailValidation){
 
         inputEmailField = document.getElementById("email");
+        iconEmailError = document.getElementById("email-error-icon");
         errorMessageContent = document.getElementById("email-error-alert");
 
         pushingAsideDiv(inputEmailField, errorMessageContent);
+        pushingAsideIcon(inputEmailField, iconEmailError);
     }
     
     if (textAreaValidation){
 
         textAreaField = document.getElementById("mensagem");
+        iconMessageError = document.getElementById("mensagem-error-icon");
         errorMessageContent = document.getElementById("mensagem-error-alert");
 
         pushingAsideDiv(textAreaField, errorMessageContent);
+        pushingAsideIcon(textAreaField, iconMessageError);
     }
 
     // if(inputNameValidation && inputEmailValidation %% textAreaValidation){
@@ -35,10 +41,20 @@ function pushingAsideDiv(field, errorContent){
     fieldWidth = field.offsetWidth;
     console.log("fieldWidth: " + fieldWidth);
 
-    marginLeft =  fieldWidth + 32;
+    marginLeft =  fieldWidth + 36;
 
     errorContent.style.marginLeft = marginLeft + "px";
     errorContent.style.visibility = "visible";
+}
+
+function pushingAsideIcon(field, icon){
+    fieldWidth = field.offsetWidth;
+    console.log("fieldWidth: " + fieldWidth);
+
+    marginLeft =  fieldWidth - 36;
+
+    icon.style.marginLeft = marginLeft + "px";
+    icon.style.visibility = "visible";
 }
 
 function inputNameValidation(){
