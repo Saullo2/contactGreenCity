3fe68f7dba61a3f4ab56298946a580d7ede53788
Saulo de Melo Macambira <saulomelo30@gmail.com>
2019-08-23 15:06:28 -0300

    A few modifications in javascript validation

--- javascript/contact.js
+++ javascript/contact.js
@@ -1,22 +1,68 @@
-window.addEventListener('resize', errorMessages);
 
-function errorMessages(){
 
-    var permission = false;
+document.getElementById("contact-form").onchange = function(){
 
-    permission = inputNameValidation();
+    nameValid = inputNameValidation();
 
-    permission = inputEmailValidation();
+    emailValid = inputEmailValidation();
 
-    permission = textAreaValidation(); 
-      
-    // if (permission) {
-    //     // all fields valid at this point
-    //     successMessage = document.getElementById("success-message");
-    //     successMessage.style.visibility = "visible";
-    // }
+    messageValid = textAreaValidation();
 
-    return permission;
+    if (nameValid && emailValid && messageValid){
+
+        btnBehavior(true);
+    } else {
+        
+        btnBehavior(false);
+    }
+
+}
+
+function errorName(){
+    nameValid = inputNameValidation();
+    
+    inputNameField = document.getElementById("nome");
+    iconNameError = document.getElementById("nome-error-icon");
+    errorMessageContent = document.getElementById("nome-error-alert");
+
+    if (nameValid) {
+
+        hidingErrorMessage(errorMessageContent, iconNameError);
+    } else {
+        pushingAsideError(inputNameField, errorMessageContent, iconNameError);
+    }
+}
+
+function errorEmail(){
+    emailValid = inputEmailValidation();
+
+    inputEmailField = document.getElementById("email");
+    iconEmailError = document.getElementById("email-error-icon");
+    errorMessageContent = document.getElementById("email-error-alert");
+
+    if (emailValid) {
+    
+        hidingErrorMessage(errorMessageContent, iconEmailError);
+    } else {
+
+        pushingAsideError(inputEmailField, errorMessageContent, iconEmailError);
+    } 
+}
+
+function errorMessage(){
+    messageValid = textAreaValidation();
+
+    textAreaField = document.getElementById("mensagem");
+    iconMessageError = document.getElementById("mensagem-error-icon");
+    errorMessageContent = document.getElementById("mensagem-error-alert");
+
+    if (messageValid) {
+    
+        hidingErrorMessage(errorMessageContent, iconMessageError);
+    } else {
+
+        pushingAsideError(textAreaField, errorMessageContent, iconMessageError);
+    } 
 }
 
 // Pushing aside and Hiding error messages
@@ -49,19 +95,13 @@ function inputNameValidation(){
     var regularExp = /^([a-z]+\s)+[a-z]+$/i;
 
     inputNameField = document.getElementById("nome");
-    iconNameError = document.getElementById("nome-error-icon");
-    errorMessageContent = document.getElementById("nome-error-alert");
 
     if (regularExp.test(inputNameField.value) && (inputNameField.value.length >= 7)){
 
         valid = true;
 
-        hidingErrorMessage(errorMessageContent, iconNameError);
-
         return valid;
     }
-
-    pushingAsideError(inputNameField, errorMessageContent, iconNameError);
 
     return valid;
 }
@@ -72,18 +112,13 @@ function inputEmailValidation(){
     var regularExp = /^[a-z0-9]+@[a-z]+\.[a-z]+(\.[a-z]+)?$/i;
 
     inputEmailField = document.getElementById("email");
-    iconEmailError = document.getElementById("email-error-icon");
-    errorMessageContent = document.getElementById("email-error-alert");
 
     if (regularExp.test(inputEmailField.value)){
 
         valid = true;
 
-        hidingErrorMessage(errorMessageContent, iconEmailError);
         return valid;
     }
-
-    pushingAsideError(inputEmailField, errorMessageContent, iconEmailError);
 
     return valid;
 }
@@ -94,18 +129,41 @@ function textAreaValidation(){
     var regularExp = /^([a-z0-9]+\s){3,}[a-z]+$/i;
 
     textAreaField = document.getElementById("mensagem");
-    iconMessageError = document.getElementById("mensagem-error-icon");
-    errorMessageContent = document.getElementById("mensagem-error-alert");
 
     if(regularExp.test(textAreaField.value) && (textAreaField.value.length >= 20)){
 
         valid = true;
 
-        hidingErrorMessage(errorMessageContent, iconMessageError);
         return valid;
     }
-    
-    pushingAsideError(textAreaField, errorMessageContent, iconMessageError);
 
     return valid;
 }
+
+// button behavior
+function btnBehavior(permission){
+
+    if(permission){
+
+        var button = document.getElementById("enviar-btn");
+        button.disabled = false;
+        button.classList.remove("enviar-btn-disabled");
+    } else {
+
+        var button = document.getElementById("enviar-btn");
+        button.disabled = true;
+        button.classList.add("enviar-btn-disabled");
+    }
+}
+
+function successMessage(){
+    successMessage = document.getElementById("success-message");
+    successMessage.visibility = "visible";
+}
+
+// document.getElementById("contact-form").addEventListener("submit", (ev) => {
+//     successMessage = document.getElementById("success-message");
+//     successMessage.visibility = "visible";
+
+//     ev.preventDefault();
+// });
